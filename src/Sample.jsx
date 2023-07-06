1cb1d5624481974ccabd79d655061c65798ac5cc
MaheshRMR <rameshandramesh11@gmail.com>
2023-07-06 09:37:58 +0530

    correction

--- src/Sample.jsx
+++ src/Sample.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import {useNavigate } from 'react-router-dom';
 import'./sample.css'
 import Movement from './movement';
-import {trans}
+
 const Sample = () => {
 
   const [Dval,setDval]=useState(0)
